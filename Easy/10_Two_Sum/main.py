979630bdf177c5df15976c798ecf1d15d8b07506
Jin Le <jiqle@ucsc.edu>
2025-09-09 15:35:10 -0700

    added solution to twosum

--- Easy/10_Two_Sum/main.py
+++ Easy/10_Two_Sum/main.py
@@ -1,45 +1,49 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-      numDict = {}
-      for index in range(0, len(nums)):
-        numberKey = nums[index]
-        numDict[numberKey] = index 
+        num_dict = {}
+        for index, value in enumerate(nums):
 
-      
-
-
-
+            if value in num_dict.keys():
+              num_dict[value].append(index)
+            else:
+              num_dict[value] = [index]
 
 
+        for index, key in enumerate(num_dict):
+          first_return_value = num_dict[key]
+          target_difference_key = target - key
+
+          # now need to find the target diference index
+          possible_second_return_value = num_dict[target_difference_key]
+          if (possible_second_return_value is not None):
+            # conditional check if same value
+            if (possible_second_return_value == first_return_value ):
+              if len(first_return_value) > 1:
+                return [first_return_value[0], possible_second_return_value[1]]
+            else: 
+              return [first_return_value[0], possible_second_return_value[0]]
 
 
 
-      
-      
 
 
+        return []
 
-      
 
-      return []
-
-      
-      
-  
 solutionTest = Solution()
 
-#nums = [2,7,11,15]
-#target = 9
-#twoSum = solutionTest.twoSum(nums, target)
-#assert twoSum == [0,1]
+nums = [2,7,11,15]
+target = 9
+twoSum = solutionTest.twoSum(nums, target)
+assert twoSum == [0,1]
 
 nums = [3, 2, 4]
 target = 6
 twoSum = solutionTest.twoSum(nums, target)
-assert twoSum == [1,2]
+assert twoSum == [1, 2]
 
-        
+
 nums = [3, 3]
 target = 6
 twoSum = solutionTest.twoSum(nums, target)
-assert twoSum == [0,1]
+assert twoSum == [0, 1]
